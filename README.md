6699246330957cfb80ce3bef2bea4d8ce37d765e
Z.F Wang <zhuofengwang2003@gmail.com>
2024-06-23 20:33:49 +0800

    Improve argument parser and update readme.

--- README.md
+++ README.md
@@ -4,3 +4,17 @@ cargo install cargo-vcpkg
 cargo vcpkg build
 cargo run
 ```
+
+### Usage
+```
+Conway's Game of Life in Rust!
+
+Usage: conway-rs [OPTIONS]
+
+Options:
+  -W, --width <WIDTH>    The width of the board [default: 32]
+  -H, --height <HEIGHT>  The height of the board [default: 32]
+  -p, --periodic         Enable periodic boundary conditions
+  -h, --help             Print help
+  -V, --version          Print version
+```
--- src/conway/args.rs
+++ src/conway/args.rs
@@ -3,14 +3,14 @@ extern crate clap;
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[command(version, about = "Conway's Game of Life in Rust!", long_about = None)]
 pub(super) struct ConwayArgs {
-    #[arg(long, default_value = "32")]
+    #[arg(long, short = 'W', default_value = "32", help = "The width of the board")]
     pub width: usize,
 
-    #[arg(long, default_value = "32")]
+    #[arg(long, short = 'H', default_value = "32", help = "The height of the board")]
     pub height: usize,
 
-    #[arg(short, long)]
+    #[arg(short, long, help = "Enable periodic boundary conditions")]
     pub periodic: bool,
 }
